e1a887328c3fbf87855da9c6b4dc763b0cdbe960
Perry Hook <perry.hook@gmail.com>
2014-06-17 13:47:24 -0700

    working on json path... parse works, get_json_search_results seems to be working.

--- basic_scraper.py
+++ basic_scraper.py
@@ -1,23 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
+import sys
+import json
 
 
-def get_search_results(query=None, minAsk=None, maxAsk=None, bedrooms=None):
-    print locals().items()
+def get_search_results(
+        query=None, minAsk=None, maxAsk=None, bedrooms=None, use_file=None):
+    # print locals().items()
     search_params = {
         key: val for key, val in locals().items() if val is not None
-    }
+        }
     if not search_params:
         raise ValueError("No valid keywords")
-
-    base = 'http://seattle.craigslist.org/search/apa'
-    resp = requests.get(base, params=search_params, timeout=3)
-    resp.raise_for_status()  # <- no-op if status==200
-    return resp.content, resp.encoding
+    if use_file:
+        with open('craigslist_response.html', 'r') as infile:
+            resp = infile.read()
+        resp.encode('utf-8')
+        return resp, 'utf-8'
+        base = 'http://seattle.craigslist.org/search/apa'
+        resp = requests.get(base, params=search_params, timeout=3)
+        resp.raise_for_status()  # <- no-op if status==200
+        return resp.content, resp.encoding
 
 
 def get_file_search_results(
         query=None, minAsk=None, maxAsk=None, bedrooms=None):
+    """
+    DEPRECATED.  USE get_search_results instead and pass it a file
+    name ifi you want it to search a file.
+    """
+
     search_params = {
         key: val for key, val in locals().items() if val is not None
     }
@@ -29,6 +41,44 @@ def get_file_search_results(
     return resp, 'utf-8'
 
 
-def parse_source(body, encoding='utf-8'):
-    parsed = BeautifulSoup(body, encoding)
-    print "parsed len is: {}".format(len(parsed))
+def get_json_search_results(
+        query=None, minAsk=None, maxAsk=None, bedrooms=None, use_file=None):
+    # print locals().items()
+    search_params = {
+        key: val for key, val in locals().items() if val is not None
+    }
+    if not search_params:
+        raise ValueError("No valid keywords")
+    if use_file:
+        with open(use_file, 'r') as infile:
+            resp = infile.read()
+            return resp
+    else:
+        base = 'http://seattle.craigslist.org/jsonsearch/apa'
+        resp = requests.get(base, params=search_params, timeout=3)
+        resp.raise_for_status()  # <- no-op if status==200
+        return resp.json()
+
+
+def parse_source(str):
+    return json.loads(str)
+
+
+def extract_json_listings(parsed):
+    all_listings = parsed[0]
+    print len(all_listings)
+
+#    return listings
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        html, encoding = get_json_search_results(
+            minAsk=500, maxAsk=1000, bedrooms=2, use_file='apa.json'
+        )
+    else:
+        html, encoding = get_json_search_results(
+            minAsk=500, maxAsk=1000, bedrooms=2
+        )
+    doc = parse_source(html, encoding)
+    print doc.prettify(encoding=encoding)
