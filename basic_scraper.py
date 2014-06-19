cce52b778b3acae2f5a8e057747eee247b76639b
Perry Hook <perry.hook@gmail.com>
2014-06-19 00:53:51 -0700

    now can load data from geoclusters.  also can reverse lookup addresses.

--- basic_scraper.py
+++ basic_scraper.py
@@ -1,57 +1,18 @@
 import requests
-# from bs4 import BeautifulSoup
 import sys
 import json
 import pprint
 
 
-def get_search_results(
-        query=None, minAsk=None, maxAsk=None, bedrooms=None, use_file=None):
-    """
-    DEPRECATED.  Just use get_json_search_results instead
-    """
-    search_params = {
-        key: val for key, val in locals().items() if val is not None
-        }
-    if not search_params:
-        raise ValueError("No valid keywords")
-    if use_file:
-        with open('craigslist_response.html', 'r') as infile:
-            resp = infile.read()
-        resp.encode('utf-8')
-        return resp, 'utf-8'
-        base = 'http://seattle.craigslist.org/search/apa'
-        resp = requests.get(base, params=search_params, timeout=3)
-        resp.raise_for_status()  # <- no-op if status==200
-        return resp.content, resp.encoding
-
-
-def get_file_search_results(
-        query=None, minAsk=None, maxAsk=None, bedrooms=None):
-    """
-    DEPRECATED.  USE get_search_results instead and pass it a file
-    name ifi you want it to search a file.
-    """
-
-    search_params = {
-        key: val for key, val in locals().items() if val is not None
-    }
-    if not search_params:
-        raise ValueError("No valid keywords")
-    with open('craigslist_response.html', 'r') as infile:
-        resp = infile.read()
-    resp.encode('utf-8')
-    return resp, 'utf-8'
-
-
 def get_json_search_results(
         query=None, minAsk=None, maxAsk=None, bedrooms=None, use_file=None):
+    # print "in get_json_search_results"
     search_params = {
         key: val for key, val in locals().items() if val is not None
     }
     if not search_params:
         raise ValueError("No valid keywords")
-    if use_file:
+    if use_file is not None:
         with open(use_file, 'r') as infile:
             resp = json.load(infile)
         return resp
@@ -62,28 +23,51 @@ def get_json_search_results(
         return resp.json()
 
 
-def extract_json_listings(parsed):
+def extract_json_geocluster_listings(geocluster):
+    # print "in geocluster listings"
+    base = 'http://seattle.craigslist.org'
+    resp = requests.get(base + geocluster['url'], timeout=3)
+    resp.raise_for_status()
+    results = resp.json()
+    listings = [{
+                'description': item['PostingTitle'],
+                'link': item['PostingURL'],
+                'location': {
+                    'data-latitude': item['Latitude'],
+                    'data-longitude': item['Longitude']
+                    },
+                'price': item['Ask'],
+                'size': item['Bedrooms']
+                } for item in results if 'PostingTitle' in item]
+    return listings
+
+
+def extract_json_listings(parsed, no_geoclusters=False):
     all_listings = parsed[0]
-    listings = [
-        {
-            'description': item['PostingTitle'],
-            'link': item['PostingURL'],
-            'location': {
-                'data-latitude': item['Latitude'],
-                'data-longitude': item['Longitude']
-                },
-            'price': item['Ask'],
-            'size': item['Bedrooms']
-        } for item in all_listings if 'PostingTitle' in item]
-    # uncomment to see what's being ditched...
-    """
+    listings = []
     for item in all_listings:
-        if 'PostingTitle' not in item:
-            print item
-    """
-#    pprint.pprint(listings)
-#    print len(all_listings)
-#    print len(listings)
+        if 'PostingTitle' in item:
+            listings.append({
+                            'description': item['PostingTitle'],
+                            'link': item['PostingURL'],
+                            'location': {
+                                'data-latitude': item['Latitude'],
+                                'data-longitude': item['Longitude']
+                                },
+                            'price': item['Ask'],
+                            'size': item['Bedrooms']
+                            })
+        # look for GeoClusters to extract (won't run if in test mode)
+        #    print "test mode is: {}".format(test_mode)
+        elif 'GeoCluster' in item and not no_geoclusters:
+            listings = listings + extract_json_geocluster_listings(item)
+        """
+        else:
+            print "This item was not extracted (this may be because you are" \
+                "running test mode):"
+            pprint.pprint(item)
+            print '\n'
+        """
     return listings
 
 
@@ -105,7 +89,7 @@ def add_address(entry):
     data = json.loads(resp.text)
     if data['status'] == 'OK':
         entry['address'] = data['results'][0]['formatted_address']
-        print entry
+        # print entry
     else:
         entry['address'] = 'unavailable'
     return entry
@@ -117,7 +101,8 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         params['use_file'] = 'apa.json'
     json_data = get_json_search_results(**params)
-    listings = extract_json_listings(json_data)
-    print len(listings)
-    pprint.pprint(listings[0])
+    # print "json_data[0][1] is: {}".format(json_data[0][1]['PostingTitle'])
+    listings = extract_json_listings(json_data, no_geoclusters=True)
+#    print len(listings)
     add_address(listings[0])
+    pprint.pprint(listings[0])
